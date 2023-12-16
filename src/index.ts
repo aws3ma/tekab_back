54d218bd7114959144e49aa748034f3d91bcdde6
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-12-16 19:11:21 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Hono } from 'hono';
 import api from './personController';
+import { cors } from 'hono/cors'
 const app = new Hono()
-
-
+app.use('/api/*', cors())
 app.get('/', (c) => {
   return c.text('Application to manage persons');
 });
